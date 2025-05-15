6ea5ccdf450ffe63935e2faf62633c4265536019
samdelaney <sdelaney@ikigaitech.org>
2025-05-15 15:59:31 -0700

    textured dial

--- app/roadmap/page.tsx
+++ app/roadmap/page.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./page.module.css";
 
 export default function RoadmapPage() {
-  var mouse = { x: 0, y: 0 };
-  var sunPos = { x: 0, y: 0 };
-  var myWidth = 0,
-    myHeight = 0;
+  const [mouse, setMouse] = useState({ x: 0, y: 0 }); // Mouse position relative to viewport
+  const [scrollY, setScrollY] = useState(0); // Scroll position
+  const animationFrameRef = useRef<number | null>(null);
+  const sunPos = useRef({ x: 0, y: 0 }); // Sun position
   var mouseIsDown = false;
   var mouseIsDownDivision = false;
-
-  const animationFrameRef = useRef<number | null>(null);
 
   function startMove() {
     mouseIsDown = true;
@@ -23,33 +21,33 @@ export default function RoadmapPage() {
     var sky = document.getElementById("sun");
   }
 
-  function updateDimensions() {
-    if (typeof window.innerWidth == "number") {
-      //Non-IE
-      myWidth = window.innerWidth;
-      myHeight = window.innerHeight;
-    } else if (
-      document.documentElement &&
-      (document.documentElement.clientWidth ||
-        document.documentElement.clientHeight)
-    ) {
-      myWidth = document.documentElement.clientWidth;
-      myHeight = document.documentElement.clientHeight;
-    } else if (
-      document.body &&
-      (document.body.clientWidth || document.body.clientHeight)
-    ) {
-      myWidth = document.body.clientWidth;
-      myHeight = document.body.clientHeight;
-    }
-  }
+  // function updateDimensions() {
+  //   if (typeof window.innerWidth == "number") {
+  //     //Non-IE
+  //     myWidth = window.innerWidth;
+  //     myHeight = window.innerHeight;
+  //   } else if (
+  //     document.documentElement &&
+  //     (document.documentElement.clientWidth ||
+  //       document.documentElement.clientHeight)
+  //   ) {
+  //     myWidth = document.documentElement.clientWidth;
+  //     myHeight = document.documentElement.clientHeight;
+  //   } else if (
+  //     document.body &&
+  //     (document.body.clientWidth || document.body.clientHeight)
+  //   ) {
+  //     myWidth = document.body.clientWidth;
+  //     myHeight = document.body.clientHeight;
+  //   }
+  // }
 
   function animateSunPosition() {
     const lerpFactor = 0.02; // Adjust this value for more or less delay
-    sunPos.x += (mouse.x - sunPos.x) * lerpFactor;
-    sunPos.y += (mouse.y - sunPos.y) * lerpFactor;
+    const combinedY = mouse.y + scrollY; // Combine mouse position and scroll offset
 
-    console.log("Sun Position:", sunPos.x, sunPos.y);
+    sunPos.current.x += (mouse.x - sunPos.current.x) * lerpFactor;
+    sunPos.current.y += (combinedY - sunPos.current.y) * lerpFactor;
 
     const sun = document.getElementById("sun");
     const sunDay = document.getElementById("sunDay");
@@ -68,90 +66,106 @@ export default function RoadmapPage() {
     const waterDistance = document.getElementById("waterDistance");
     const sky = document.getElementById("sky");
     const shadow = document.getElementById("shadow");
+    const dial = document.getElementById("dial");
 
     const bodyWidth = document.body.clientWidth;
     const myHeight = window.innerHeight;
+    const sunHeightPct = sunPos.current.y / myHeight;
+    const sunXPct = sunPos.current.x / bodyWidth;
 
     if (shadow) {
       shadow.style.clipPath = `polygon(400px 0px, 500px 0px, ${
-        975 - sunPos.x
-      }px ${sunPos.y / 3}px)`;
-      shadow.style.opacity = `${1 - sunPos.y / myHeight}`;
+        975 - sunPos.current.x
+      }px ${sunPos.current.y / 3}px)`;
+      shadow.style.opacity = `${1 - sunHeightPct}`;
+    }
+
+    if (dial) {
+      // Calculate gradient based on sun's position
+      const ygradientStart = `rgba(255, 183, 11, 1) ${
+        100 * (1 - sunHeightPct * (sunXPct ^ 2) * 0.5)
+      }%`;
+
+      const gradientEnd = `rgba(0, 0, 0, 0.7) 100%`;
+
+      // Apply gradient to the dial
+      dial.style.background = `linear-gradient(115deg, ${ygradientStart}, ${gradientEnd})`;
+      // dial.style.clipPath = "polygon(100% 0%, 0% 100%, 100% 100%)"; // Ensure the gradient matches the triangle shape
     }
 
     if (sun) {
-      sun.style.background = `-webkit-radial-gradient(${sunPos.x}px ${sunPos.y}px, circle, rgba(242,248,247,1) 0%,rgba(249,249,28,1) 3%,rgba(247,214,46,1) 8%, rgba(248,200,95,1) 12%,rgba(201,165,132,1) 30%,rgba(115,130,133,1) 51%,rgba(46,97,122,1) 85%,rgba(24,75,106,1) 100%)`;
-      sun.style.background = `-moz-radial-gradient(${sunPos.x}px ${sunPos.y}px, circle, rgba(242,248,247,1) 0%,rgba(249,249,28,1) 3%,rgba(247,214,46,1) 8%, rgba(248,200,95,1) 12%,rgba(201,165,132,1) 30%,rgba(115,130,133,1) 51%,rgba(46,97,122,1) 85%,rgba(24,75,106,1) 100%)`;
-      sun.style.background = `-ms-radial-gradient(${sunPos.x}px ${sunPos.y}px, circle, rgba(242,248,247,1) 0%,rgba(249,249,28,1) 3%,rgba(247,214,46,1) 8%, rgba(248,200,95,1) 12%,rgba(201,165,132,1) 30%,rgba(115,130,133,1) 51%,rgba(46,97,122,1) 85%,rgba(24,75,106,1) 100%)`;
+      sun.style.background = `-webkit-radial-gradient(${sunPos.current.x}px ${sunPos.current.y}px, circle, rgba(242,248,247,1) 0%,rgba(249,249,28,1) 3%,rgba(247,214,46,1) 8%, rgba(248,200,95,1) 12%,rgba(201,165,132,1) 30%,rgba(115,130,133,1) 51%,rgba(46,97,122,1) 85%,rgba(24,75,106,1) 100%)`;
+      sun.style.background = `-moz-radial-gradient(${sunPos.current.x}px ${sunPos.current.y}px, circle, rgba(242,248,247,1) 0%,rgba(249,249,28,1) 3%,rgba(247,214,46,1) 8%, rgba(248,200,95,1) 12%,rgba(201,165,132,1) 30%,rgba(115,130,133,1) 51%,rgba(46,97,122,1) 85%,rgba(24,75,106,1) 100%)`;
+      sun.style.background = `-ms-radial-gradient(${sunPos.current.x}px ${sunPos.current.y}px, circle, rgba(242,248,247,1) 0%,rgba(249,249,28,1) 3%,rgba(247,214,46,1) 8%, rgba(248,200,95,1) 12%,rgba(201,165,132,1) 30%,rgba(115,130,133,1) 51%,rgba(46,97,122,1) 85%,rgba(24,75,106,1) 100%)`;
       sun.style.width = `${bodyWidth}px`;
       sun.style.left = "0px";
     }
 
     if (sunDay) {
-      sunDay.style.background = `-webkit-radial-gradient(${sunPos.x}px ${sunPos.y}px, circle, rgba(252,255,251,0.9) 0%,rgba(253,250,219,0.4) 30%,rgba(226,219,197,0.01) 70%, rgba(226,219,197,0.0) 70%,rgba(201,165,132,0) 100%)`;
-      sunDay.style.background = `-moz-radial-gradient(${sunPos.x}px ${sunPos.y}px, circle, rgba(252,255,251,0.9) 0%,rgba(253,250,219,0.4) 30%,rgba(226,219,197,0.01) 70%, rgba(226,219,197,0.0) 70%,rgba(201,165,132,0) 100%)`;
-      sunDay.style.background = `-ms-radial-gradient(${sunPos.x}px ${sunPos.y}px, circle, rgba(252,255,251,0.9) 0%,rgba(253,250,219,0.4) 30%,rgba(226,219,197,0.01) 70%, rgba(226,219,197,0.0) 70%,rgba(201,165,132,0) 100%)`;
+      sunDay.style.background = `-webkit-radial-gradient(${sunPos.current.x}px ${sunPos.current.y}px, circle, rgba(252,255,251,0.9) 0%,rgba(253,250,219,0.4) 30%,rgba(226,219,197,0.01) 70%, rgba(226,219,197,0.0) 70%,rgba(201,165,132,0) 100%)`;
+      sunDay.style.background = `-moz-radial-gradient(${sunPos.current.x}px ${sunPos.current.y}px, circle, rgba(252,255,251,0.9) 0%,rgba(253,250,219,0.4) 30%,rgba(226,219,197,0.01) 70%, rgba(226,219,197,0.0) 70%,rgba(201,165,132,0) 100%)`;
+      sunDay.style.background = `-ms-radial-gradient(${sunPos.current.x}px ${sunPos.current.y}px, circle, rgba(252,255,251,0.9) 0%,rgba(253,250,219,0.4) 30%,rgba(226,219,197,0.01) 70%, rgba(226,219,197,0.0) 70%,rgba(201,165,132,0) 100%)`;
       sunDay.style.width = `${bodyWidth}px`;
       sunDay.style.left = "0px";
-      sunDay.style.opacity = `${1 - sunPos.y / myHeight}`;
+      sunDay.style.opacity = `${1 - sunHeightPct}`;
     }
 
     if (sunSet) {
-      sunSet.style.background = `-webkit-radial-gradient(${sunPos.x}px ${sunPos.y}px, circle, rgba(254,255,255,0.8) 5%,rgba(236,255,0,1) 10%,rgba(253,50,41,1) 25%, rgba(243,0,0,1) 40%,rgba(93,0,0,1) 100%)`;
-      sunSet.style.background = `-moz-radial-gradient(${sunPos.x}px ${sunPos.y}px, circle, rgba(254,255,255,0.8) 5%,rgba(236,255,0,1) 10%,rgba(253,50,41,1) 25%, rgba(243,0,0,1) 40%,rgba(93,0,0,1) 100%)`;
-      sunSet.style.background = `-ms-radial-gradient(${sunPos.x}px ${sunPos.y}px, circle, rgba(254,255,255,0.8) 5%,rgba(236,255,0,1) 10%,rgba(253,50,41,1) 25%, rgba(243,0,0,1) 40%,rgba(93,0,0,1) 100%)`;
-      sunSet.style.opacity = `${sunPos.y / myHeight - 0.2}`;
+      sunSet.style.background = `-webkit-radial-gradient(${sunPos.current.x}px ${sunPos.current.y}px, circle, rgba(254,255,255,0.8) 5%,rgba(236,255,0,1) 10%,rgba(253,50,41,1) 25%, rgba(243,0,0,1) 40%,rgba(93,0,0,1) 100%)`;
+      sunSet.style.background = `-moz-radial-gradient(${sunPos.current.x}px ${sunPos.current.y}px, circle, rgba(254,255,255,0.8) 5%,rgba(236,255,0,1) 10%,rgba(253,50,41,1) 25%, rgba(243,0,0,1) 40%,rgba(93,0,0,1) 100%)`;
+      sunSet.style.background = `-ms-radial-gradient(${sunPos.current.x}px ${sunPos.current.y}px, circle, rgba(254,255,255,0.8) 5%,rgba(236,255,0,1) 10%,rgba(253,50,41,1) 25%, rgba(243,0,0,1) 40%,rgba(93,0,0,1) 100%)`;
+      sunSet.style.opacity = `${sunHeightPct - 0.2}`;
     }
 
     if (waterReflectionContainer) {
       waterReflectionContainer.style.perspectiveOrigin = `${
-        (sunPos.x / bodyWidth) * 100
+        (sunPos.current.x / bodyWidth) * 100
       }% -15%`;
     }
 
     if (waterReflectionMiddle) {
       waterReflectionMiddle.style.left = `${
-        sunPos.x - bodyWidth - bodyWidth * 0.03
+        sunPos.current.x - bodyWidth - bodyWidth * 0.03
       }px`;
     }
 
     if (darknessOverlay) {
       darknessOverlay.style.opacity = `${Math.min(
-        (sunPos.y - myHeight / 2) / (myHeight / 2),
+        (sunPos.current.y - myHeight / 1.9) / (myHeight / 2),
         1
       )}`;
     }
 
     if (darknessOverlaySky) {
       darknessOverlaySky.style.opacity = `${Math.min(
-        (sunPos.y - myHeight * 0.7) / (myHeight - myHeight * 0.7),
+        (sunPos.current.y - myHeight * 0.7) / (myHeight - myHeight * 0.7),
         1
       )}`;
     }
 
     if (moon) {
       moon.style.opacity = `${Math.min(
-        (sunPos.y - myHeight * 0.9) / (myHeight - myHeight * 0.9),
+        (sunPos.current.y - myHeight * 0.9) / (myHeight - myHeight * 0.9),
         0.65
       )}`;
     }
 
     if (horizonNight) {
       horizonNight.style.opacity = `${
-        (sunPos.y - myHeight * 0.8) / (myHeight - myHeight * 0.8)
+        (sunPos.current.y - myHeight * 0.8) / (myHeight - myHeight * 0.8)
       }`;
     }
 
     if (starsContainer) {
-      starsContainer.style.opacity = `${sunPos.y / myHeight - 0.6}`;
+      starsContainer.style.opacity = `${sunHeightPct - 0.6}`;
     }
 
     if (waterDistance) {
-      waterDistance.style.opacity = `${sunPos.y / myHeight + 0.6}`;
+      waterDistance.style.opacity = `${sunHeightPct + 0.6}`;
     }
 
     if (sky) {
-      sky.style.opacity = `${Math.min(1 - sunPos.y / myHeight, 0.99)}`;
+      sky.style.opacity = `${Math.min(1 - sunHeightPct, 0.99)}`;
     }
 
     animationFrameRef.current = requestAnimationFrame(animateSunPosition);
@@ -160,27 +174,29 @@ export default function RoadmapPage() {
   useEffect(() => {
     // Start the animation loop
     animationFrameRef.current = requestAnimationFrame(animateSunPosition);
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
 
     // Cleanup on component unmount
     return () => {
+      window.removeEventListener("scroll", handleScroll);
       if (animationFrameRef.current) {
         cancelAnimationFrame(animationFrameRef.current);
       }
     };
-  }, []);
+  }, [mouse, scrollY]);
 
   function handleMouseMove(
     event: React.MouseEvent<HTMLDivElement> | React.WheelEvent<HTMLDivElement>
   ) {
     // if (!mouseIsDown) return;
-    console.log("Mouse Move:", event.clientX, event.clientY);
-
-    mouse.x = event.clientX;
-    mouse.y = event.clientY + window.scrollY;
-  }
-
-  function startDraggingDivision() {
-    mouseIsDownDivision = true;
+    setMouse({
+      x: event.clientX,
+      y: event.clientY,
+    });
   }
 
   return (
@@ -270,13 +286,6 @@ export default function RoadmapPage() {
         id="shadow"
         className={styles.shadow}
         onMouseDown={startMove}
-        onMouseUp={stopMove}
-      ></div>
-
-      <div
-        id="division"
-        className={styles.division}
-        onMouseDown={startDraggingDivision}
         onMouseUp={stopMove}
       ></div>
 
