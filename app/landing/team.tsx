dc590e6ef8a8c597e07ca2f61fdab0b14702f448
samdelaney <sam@thedelaneys.net>
2025-06-07 20:08:23 -0700

    lil cleanup

--- app/landing/team.tsx
+++ app/landing/team.tsx
@@ -1,4 +1,5 @@
 import { Section } from "@/components/ui/section";
+import Image from "next/image";
 
 function TeamMember({
   name,
@@ -18,7 +19,13 @@ function TeamMember({
       href={link}
       className="flex flex-col items-center text-center p-6 rounded-lg shadow-sm hover:shadow-primary/60 transition-shadow"
     >
-      <img src={image} alt={name} className="w-36 h-36 rounded-full mb-4" />
+      <Image
+        src={image}
+        alt={name}
+        height={100}
+        width={100}
+        className="w-36 h-36 rounded-full mb-4"
+      />
       <h2 className="text-2xl font-bold text-primary">{name}</h2>
       <p>{role}</p>
       <p className="text-gray-500 text-sm">{description ?? ""}</p>
