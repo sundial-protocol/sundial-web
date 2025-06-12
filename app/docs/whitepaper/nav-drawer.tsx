c7363ecfd6409cc7a5c6e8aa7ac61e0b0b01bcf5
samdelaney <sam@thedelaneys.net>
2025-06-12 12:14:52 -0700

    whitepaper nav drawer

---
+++ app/docs/whitepaper/nav-drawer.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { SidebarCloseIcon, TableOfContents } from "lucide-react";
+import { useState } from "react";
+import { Drawer } from "vaul";
+
+export default function NavDrawer() {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <Drawer.Root open={open} direction="left">
+      <Drawer.Trigger
+        onClick={() => setOpen(!open)}
+        className={`mt-36 z-10 top-4 fixed rounded-full ${
+          open ? "bg-primary/70" : "bg-primary"
+        } p-4 text-sm font-medium text-black shadow-sm transition-all hover:bg-primary/70`}
+      >
+        <TableOfContents className="h-5 w-5" />
+      </Drawer.Trigger>
+      <Drawer.Portal>
+        <Drawer.Content
+          className="left-2 top-2 bottom-2 py-36 fixed z-20 outline-none w-[310px] flex"
+          // The gap between the edge of the screen and the drawer is 8px in this case.
+          style={
+            { "--initial-transform": "calc(100% + 8px)" } as React.CSSProperties
+          }
+        >
+          <div className="bg-background border-primary/50 border min-h-[200px] h-full w-full grow p-5 flex flex-col rounded-[16px]">
+            <div className="max-w-md mx-auto">
+              <Drawer.Title className="font-medium mb-2 text-foreground">
+                It supports all directions.
+              </Drawer.Title>
+              <Drawer.Description className="text-foreground mb-2">
+                This one specifically is not touching the edge of the screen,
+                but that&apos;s not required for a side drawer.
+              </Drawer.Description>
+            </div>
+          </div>
+          <Drawer.Close
+            onClick={() => setOpen(false)}
+            className="mt-36 top-4 right-2 fixed rounded-full z-10 bg-background/70 border-2 border-primary p-2 text-sm font-medium text-primary shadow-sm transition-all hover:bg-background"
+          >
+            <SidebarCloseIcon className="h-5 w-5" />
+          </Drawer.Close>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
+  );
+}
--- app/docs/whitepaper/page.tsx
+++ app/docs/whitepaper/page.tsx
@@ -4,6 +4,7 @@ import { Section } from "@/components/ui/section";
 import { pdfjs, Document, Page } from "react-pdf";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import NavDrawer from "./nav-drawer";
 
 // Set the workerSrc to the correct path
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
@@ -11,14 +12,15 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/b
 export default function Whitepaper() {
   return (
     <Section>
-      <div className="flex items-center justify-center">
+      <NavDrawer />
+      <div className="flex items-center justify-center -top-10">
         <Document
           file="/sundial.pdf"
           loading="Loading document..."
-          className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center justify-center"
+          className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center justify-center z-0"
         >
           {Array.from({ length: 99 }, (_, i) => (
-            <Page key={i + 1} pageNumber={i + 1} width={500} />
+            <Page key={i + 1} pageNumber={i + 1} width={500} className="z-0" />
           ))}
         </Document>
       </div>
