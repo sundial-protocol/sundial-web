f86d1d742c5ab0c3f179d53246c4cd2a5668f226
samdelaney <sam@thedelaneys.net>
2025-06-27 12:50:42 -0700

    add bg logos

--- app/landing/advanced-features.tsx
+++ app/landing/advanced-features.tsx
@@ -21,13 +21,23 @@ export function FeatureCard({
 }) {
   return (
     <div
-      className={`flex flex-col justify-center p-6 bg-secondary rounded-sm shadow-sm hover:shadow-primary/60 hover:shadow-md transition-shadow ${
+      className={`relative flex flex-col justify-center p-6 bg-secondary/90 rounded-sm shadow-sm hover:shadow-primary/60 hover:shadow-md transition-shadow overflow-hidden ${
         className || ""
       }`}
     >
-      {Icon && <Icon className={"text-gray-500 h-12 w-12 pb-4"} />}
-      <h2 className={"text-lg text-primary font-semibold"}>{title}</h2>
-      <p className={"text-gray-500"}>{description}</p>
+      {/* Background Icon */}
+      {Icon && (
+        <Icon
+          className="absolute -bottom-10 -right-10 m-auto opacity-30 text-gray-500 h-[150px] w-[150px] pointer-events-none select-none"
+          aria-hidden="true"
+        />
+      )}
+      {/* Foreground Content */}
+      {Icon && <Icon className="text-gray-500 h-12 w-12 pb-4 relative z-10" />}
+      <h2 className="text-lg text-primary font-semibold relative z-10">
+        {title}
+      </h2>
+      <p className="text-gray-500 relative z-10">{description}</p>
     </div>
   );
 }
@@ -83,14 +93,14 @@ export function AdvancedFeatures() {
         },
       ]}
     >
-      <Section>
+      <Section className="min-h-screen flex flex-col items-center justify-center">
         <div className="grid grid-cols-1 lg:grid-cols-3 container px-4 md:px-6 z-10">
           <div className="flex flex-row justify-center space-y-4">
             <div className="space-y-2 p-6">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
                 Advanced Features
               </h1>
-              <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl">
+              <p className="mx-auto max-w-[700px] text-secondary-foreground md:text-xl">
                 Sundial offers a unique set of features that maximize your
                 Bitcoin yields while maintaining security.
               </p>
