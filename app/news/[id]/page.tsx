1c4bab154502f8241b8ecf60573d52d79bc90504
samdelaney <sdelaney@ikigaitech.org>
2025-08-01 20:47:51 -0700

    news articles content

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -16,19 +16,18 @@ export default async function NewsPage({
   return (
     <Section>
       {article ? (
-        <div className="max-w-2xl mx-auto">
-          <h1 className="text-6xl font-bold mb-4">{article.title}</h1>
-          <p className="text-sm text-gray-500 mb-2">{article.date}</p>
-          <p className="mb-6">{article.description}</p>
+        <div className="max-w-2xl mx-auto py-6">
+          <h1 className="text-6xl font-bold m-4">{article.title}</h1>
+          <p className="text-sm text-gray-500 m-4">{article.date}</p>
           <div>
-            <Image
-              src={article.image}
-              alt={article.title}
-              className="w-full h-auto rounded-lg mb-6"
-              width={1000}
-              height={1000}
-            />
-            <div className="prose prose-lg max-w-none py-12">
+            <div className="prose prose-lg max-w-none">
+              <Image
+                src={article.image}
+                alt={article.title}
+                className="w-full h-auto rounded-lg mb-6"
+                width={1000}
+                height={1000}
+              />
               {article.content ? (
                 <ReactMarkdown
                   remarkPlugins={[remarkGfm]}
