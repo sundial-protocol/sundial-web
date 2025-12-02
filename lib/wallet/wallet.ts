e6fbb41250fc37a17994e71ff08f217631486fe4
samdelaney <sam@thedelaneys.net>
2025-12-01 20:27:07 -0800

    upgrade to next v16

--- lib/wallet/wallet.ts
+++ lib/wallet/wallet.ts
@@ -22,6 +22,7 @@ export function getInstalledWalletExtensions(supportedWallets?: string[]) {
     .filter(
       (walletExtension) =>
         walletExtension !== "typhon" &&
+        !!cardano[walletExtension].enable &&
         typeof cardano[walletExtension].enable === "function"
     )
     .map((walletExtension) => walletExtension.toLowerCase());
