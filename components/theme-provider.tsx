42fefbb683ea9688ceae0cc7d5f02654b87f0981
samdelaney <sam@thedelaneys.net>
2026-07-27 10:24:36 -0700

    render ThemeProvider directly

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,26 +1,15 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { useState, useEffect } from "react";
 
 export default function CustomThemeProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>; // Render children without ThemeProvider during SSR
-  }
-
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
       {children}
     </ThemeProvider>
-  ); // Wrap children with ThemeProvider after mount
+  );
 }
